4bd4460eba05f44c2018ea8d9cc86aa9586fca6a
Chaitanya Negi <118297902+chaitanyasinghnegi@users.noreply.github.com>
2023-12-26 22:57:34 +0530

    Update check-type.py

--- check-type.py
+++ check-type.py
@@ -1,1 +1,7 @@
+"""
+This file will consist of different types of checks we can do 
+in python. It includes special numbers like Armstrong number,
+Palindromes, Magic number, counter of typecases etc.
+"""
+
 
