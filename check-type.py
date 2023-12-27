8d4bcb769cc104739fcbef16fac52fb3b2b9934b
Chaitanya Negi <118297902+chaitanyasinghnegi@users.noreply.github.com>
2023-12-27 21:47:52 +0530

    updated check-type.py

--- check-type.py
+++ check-type.py
@@ -4,4 +4,10 @@ in python. It includes special numbers like Armstrong number,
 Palindromes, Magic number, counter of typecases etc.
 """
 
-
+"""
+Armstrong number checker
+input : integer
+output : boolean
+checks if the number given is equal to the sum of power of each digit of the number to the number of digits
+example 
+"""
