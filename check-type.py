6287c5d9191251e7385281312fb31f4b22f3607c
Chaitanya Negi <118297902+chaitanyasinghnegi@users.noreply.github.com>
2023-12-27 21:49:44 +0530

    Update check-type.py

--- check-type.py
+++ check-type.py
@@ -9,5 +9,6 @@ Armstrong number checker
 input : integer
 output : boolean
 checks if the number given is equal to the sum of power of each digit of the number to the number of digits
-example 
+example- 153 = 1^3 + 5^3 + 3^3  (number of digits = 3)
+         1634 = 1^4 + 6^4 + 3^4 + 4^4  (number of digits = 4)
 """
