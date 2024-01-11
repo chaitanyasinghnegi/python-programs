a1b05f8bcfb37ea7a82ea8a0e35809103f821d30
Chaitanya Negi <chaitanyanegi1@gmail.com>
2024-01-11 19:08:40 +0530

    renaming and fixing the comment

--- my-random.py
+++ my-random.py
@@ -3,4 +3,4 @@ This file is a simple program that
 basically randomizes integers between two given intervals, 
 using the linear congruential algorithm with variable seeds 
 from time, date, os files etc.
-""
+"""
