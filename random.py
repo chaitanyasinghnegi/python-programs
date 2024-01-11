11ae35cdbc1ce14f96916b8c5eee477a87827bfb
Chaitanya Negi <118297902+chaitanyasinghnegi@users.noreply.github.com>
2024-01-11 19:05:30 +0530

    Update random.py

--- random.py
+++ random.py
@@ -1,1 +1,6 @@
-
+"""
+This file is a simple program that 
+basically randomizes integers between two given intervals, 
+using the linear congruential algorithm with variable seeds 
+from time, date, os files etc.
+""
